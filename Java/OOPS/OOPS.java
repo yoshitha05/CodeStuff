604529bb717e0b776ad63570785badf2ec0cb0dd
YOSHITHA P <142377316+yoshitha05@users.noreply.github.com>
2025-02-23 15:13:59 +0530

    Create OOPS.java

---
+++ Java/OOPS/OOPS.java
@@ -0,0 +1,30 @@
+class Student {
+    String name;
+    int age;
+
+    public void printInfo() {
+        System.out.println(this.name);
+        System.out.println(this.age);
+    }
+
+    // Non parameterized Constructor
+    // Student() {
+    //     System.out.println("constructor called");
+    // }
+
+    // parameterized Constructor
+    Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+public class OOPS {
+    public static void main(String[] args) {
+        Student s1 = new Student("YOSHITHA", 19);
+        // s1.name = "YOSHITHA";
+        // s1.age = 19;
+
+        s1.printInfo();
+    }
+}
