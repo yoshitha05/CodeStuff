780372325f1838c1b54e2730eb900c0a3c126d88
YOSHITHA P <142377316+yoshitha05@users.noreply.github.com>
2025-03-11 22:35:57 +0530

    [29] [Time Beats: 100.00%] [Memory Beats: 14.72%] - LeetPush

---
+++ leetcode/Divide-Two-Integers.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def divide(self, dividend, divisor):
+        \\\
+        :type dividend: int
+        :type divisor: int
+        :rtype: int
+        \\\
+        if dividend == -2**31 and divisor == -1:
+            return 2**31 - 1  # Clamped to 32-bit signed integer range
+
+        # Get sign of result
+        sign = -1 if (dividend < 0) ^ (divisor < 0) else 1  
+
+        # Work with absolute values
+        dividend, divisor = abs(dividend), abs(divisor)
+        quotient = 0
+
+        while dividend >= divisor:
+            temp, multiple = divisor, 1
+            while dividend >= (temp << 1):  # Double until just below dividend
+                temp <<= 1
+                multiple <<= 1
+
+            dividend -= temp  # Subtract the biggest chunk possible
+            quotient += multiple  # Add corresponding multiple to quotient
+
+        return sign * quotient 
