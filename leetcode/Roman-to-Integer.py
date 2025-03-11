b03026e6ab16e6f74dbb1602afee01f13147ba91
YOSHITHA P <142377316+yoshitha05@users.noreply.github.com>
2025-03-11 22:51:58 +0530

    Solved Roman-to-Integer.py

--- leetcode/Roman-to-Integer.py
+++ leetcode/Roman-to-Integer.py
@@ -4,7 +4,7 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        roman_map = {
+        roman_map = { 
             'I': 1, 'V': 5, 'X': 10, 'L': 50,
             'C': 100, 'D': 500, 'M': 1000
         }
