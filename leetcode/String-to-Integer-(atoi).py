c3b02e27208a320b86abe83aa3b9d9b8dbd12102
YOSHITHA P <142377316+yoshitha05@users.noreply.github.com>
2025-03-11 22:11:30 +0530

    [8] [Time Beats: 33.52%] [Memory Beats: 85.97%] - LeetPush

---
+++ leetcode/String-to-Integer-(atoi).py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def myAtoi(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        s = s.lstrip()  # Remove leading whitespace
+        if not s:
+            return 0
+
+        sign = 1
+        index = 0
+        if s[index] == "-":
+            sign = -1
+            index += 1
+        elif s[index] == "+":
+            index += 1
+
+        result = 0
+        while index < len(s) and s[index].isdigit():
+            result = result * 10 + int(s[index])
+            index += 1
+
+        result *= sign
+
+        # Clamp result to 32-bit integer range
+        int_min, int_max = -2**31, 2**31 - 1
+        if result < int_min:
+            return int_min
+        if result > int_max:
+            return int_max
+
+        return result
