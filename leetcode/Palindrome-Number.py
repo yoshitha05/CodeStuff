1d22253a3afda61aa0e920b1ba920b31f3dd6f19
YOSHITHA P <142377316+yoshitha05@users.noreply.github.com>
2025-03-11 22:50:06 +0530

    Solved Palindrome-Number.py

--- leetcode/Palindrome-Number.py
+++ leetcode/Palindrome-Number.py
@@ -10,7 +10,7 @@ class Solution(object):
         # Reversing half of the number
         reversed_half = 0
         while x > reversed_half:
-            reversed_half = reversed_half * 10 + x % 10
+            reversed_half = reversed_half * 10 + x % 10 
             x //= 10
         
         # A number is a palindrome if the original number (or the truncated half)
