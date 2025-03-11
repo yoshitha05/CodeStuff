ed9003ecb732da85bd6f5752f8dffa5b1fcf8a41
YOSHITHA P <142377316+yoshitha05@users.noreply.github.com>
2025-03-11 22:09:04 +0530

    [5] [Time Beats: 58.00%] [Memory Beats: 80.68%] - LeetPush

---
+++ leetcode/Longest-Palindromic-Substring.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        if not s or len(s) == 1:
+            return s
+        
+        def expand_around_center(left, right):
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            return s[left + 1:right]
+        
+        longest = ""
+        for i in range(len(s)):
+            odd_palindrome = expand_around_center(i, i)  # Odd length palindrome
+            even_palindrome = expand_around_center(i, i + 1)  # Even length palindrome
+            
+            if len(odd_palindrome) > len(longest):
+                longest = odd_palindrome
+            if len(even_palindrome) > len(longest):
+                longest = even_palindrome
+        
+        return longest
