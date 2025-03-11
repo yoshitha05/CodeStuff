e8ffb35874df2a6027f010f8b185b3956cc64a2b
YOSHITHA P <142377316+yoshitha05@users.noreply.github.com>
2025-03-11 22:39:51 +0530

    [33] [Time Beats: 100.00%] [Memory Beats: 70.39%] - LeetPush

---
+++ leetcode/Search-in-Rotated-Sorted-Array.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def search(self, nums, target):
+        \\\
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        \\\
+        left, right = 0, len(nums) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if nums[mid] == target:
+                return mid
+
+            # Check if left half is sorted
+            if nums[left] <= nums[mid]:
+                if nums[left] <= target < nums[mid]:  # Target in left half
+                    right = mid - 1
+                else:  # Target in right half
+                    left = mid + 1
+            else:  # Right half is sorted
+                if nums[mid] < target <= nums[right]:  # Target in right half
+                    left = mid + 1
+                else:  # Target in left half
+                    right = mid - 1
+
+        return -1  # Target not found
