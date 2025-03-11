4204efd77aa4ab7daee1d1be5a258b7ebbbeb91f
YOSHITHA P <142377316+yoshitha05@users.noreply.github.com>
2025-03-11 22:35:03 +0530

    [27] [Time Beats: 100.00%] [Memory Beats: 80.33%] - LeetPush

---
+++ leetcode/Remove-Element.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        \\\
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        \\\
+        i = 0  # Slow pointer for placement of non-val elements
+        for j in range(len(nums)):  # Fast pointer to scan elements
+            if nums[j] != val:
+                nums[i] = nums[j]  # Place the non-val element at index i
+                i += 1  # Move the slow pointer forward
+        return i  # New length of modified array
