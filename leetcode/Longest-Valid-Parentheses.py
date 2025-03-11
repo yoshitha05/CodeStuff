bc3bed5bdef588222045b1849965de7a966c82eb
YOSHITHA P <142377316+yoshitha05@users.noreply.github.com>
2025-03-11 22:38:50 +0530

    [32] [Time Beats: 81.42%] [Memory Beats: 55.06%] - LeetPush

---
+++ leetcode/Longest-Valid-Parentheses.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def longestValidParentheses(self, s):
+        \\\
+        :type s: str
+        :rtype: int
+        \\\
+        stack = [-1]  # Initialize stack with -1 for base case
+        max_length = 0
+        
+        for i in range(len(s)):
+            if s[i] == '(':
+                stack.append(i)  # Push index of '('
+            else:
+                stack.pop()  # Pop previous '(' or invalid ')'
+                if not stack:
+                    stack.append(i)  # Push current index as new invalid base
+                else:
+                    max_length = max(max_length, i - stack[-1])  # Compute max length
+        
+        return max_length
