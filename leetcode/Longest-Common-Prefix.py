b923675b061499f52df466320689c544ac2dec66
YOSHITHA P <142377316+yoshitha05@users.noreply.github.com>
2025-03-11 22:16:57 +0530

    [14] [Time Beats: 100.00%] [Memory Beats: 60.90%] - LeetPush

---
+++ leetcode/Longest-Common-Prefix.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs:
+            return ""
+        
+        strs.sort()  # Sort the list alphabetically
+        first, last = strs[0], strs[-1]
+        
+        i = 0
+        while i < len(first) and i < len(last) and first[i] == last[i]:
+            i += 1
+        
+        return first[:i]
