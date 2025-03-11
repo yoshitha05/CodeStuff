b2a1c43a497b3fb2e64096d96fd55fa531111ddf
YOSHITHA P <142377316+yoshitha05@users.noreply.github.com>
2025-03-11 21:29:39 +0530

    [69] [Time Beats: 100.00%] [Memory Beats: 80.55%] - LeetPush

---
+++ leetcode/Sqrt(x).py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if x == 0:
+            return 0
+        
+        left, right = 0, x
+        while left <= right:
+            mid = (left + right) // 2
+            if mid * mid == x:
+                return mid
+            elif mid * mid < x:
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        return right
+        
