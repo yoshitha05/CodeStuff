61059ecb051ce639a670d97792df406aabb0f144
YOSHITHA P <142377316+yoshitha05@users.noreply.github.com>
2025-03-11 21:26:26 +0530

    [7] [Time Beats: 28.18%] [Memory Beats: 76.91%] - LeetPush

---
+++ leetcode/Reverse-Integer.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        sign = -1 if x < 0 else 1
+        x = abs(x)
+        
+        # Reverse the digits of the integer
+        reversed_x = int(str(x)[::-1])
+        
+        # Reapply the sign
+        reversed_x *= sign
+        
+        # Handle overflow for 32-bit signed integers
+        if reversed_x < -2**31 or reversed_x > 2**31 - 1:
+            return 0
+        
+        return reversed_x
+        
