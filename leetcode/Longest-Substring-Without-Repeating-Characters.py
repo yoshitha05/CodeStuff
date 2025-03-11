287754ded89fc2da36d6fdd104b837d99f5c9855
YOSHITHA P <142377316+yoshitha05@users.noreply.github.com>
2025-03-11 22:06:43 +0530

    [3] [Time Beats: 69.43%] [Memory Beats: 15.58%] - LeetPush

---
+++ leetcode/Longest-Substring-Without-Repeating-Characters.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        char_index = {}  # Dictionary to store the index of characters
+        left = 0  # Left pointer of the sliding window
+        max_length = 0  # Maximum length of substring without repeating characters
+
+        for right in range(len(s)):
+            if s[right] in char_index and char_index[s[right]] >= left:
+                left = char_index[s[right]] + 1  # Move left pointer past the repeated character
+            
+            char_index[s[right]] = right  # Update character index
+            max_length = max(max_length, right - left + 1)  # Update max length
+
+        return max_length
+        
