3700911dbf5ee4ad0b21a41c202092fe77a70bd5
YOSHITHA P <142377316+yoshitha05@users.noreply.github.com>
2025-03-11 22:14:26 +0530

    [11] [Time Beats: 53.13%] [Memory Beats: 14.21%] - LeetPush

---
+++ leetcode/Container-With-Most-Water.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        left, right = 0, len(height) - 1  # Initialize the two pointers
+        max_water = 0  # Variable to track maximum area
+
+        while left < right:
+            # Calculate the current area
+            min_height = min(height[left], height[right])
+            width = right - left
+            max_water = max(max_water, min_height * width)
+
+            # Move the pointer with the smaller height
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+
+        return max_water
