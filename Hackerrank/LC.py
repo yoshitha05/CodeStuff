3096e5251ff6799d756a7f3d414ec70b38d00945
YOSHITHA P <142377316+yoshitha05@users.noreply.github.com>
2025-03-12 07:35:53 +0530

    Solved LC.py

--- Hackerrank/LC.py
+++ Hackerrank/LC.py
@@ -4,4 +4,4 @@ if __name__ == '__main__':
     z = int(input())
     n = int(input())
     out = [[i, j, k] for i in range(0,x+1) for j in range(y+1) for k in range(z+1) if i+j+k != n]
-    print(out)
+    print(out) 
