6c02860fba099dd31305f24fb1b0c842b6193964
YOSHITHA P <142377316+yoshitha05@users.noreply.github.com>
2025-03-12 09:40:42 +0530

    [41] [Time Beats: 39.73%] [Memory Beats: 75.21%] - LeetPush

---
+++ leetcode/First-Missing-Positive.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def firstMissingPositive(self, nums):
+        \\\
+        :type nums: List[int]
+        :rtype: int
+        \\\
+        n = len(nums)
+        
+        # Step 1: Place each number in its correct index (Cyclic Sort)
+        for i in range(n):
+            while 1 <= nums[i] <= n and nums[nums[i] - 1] != nums[i]:
+                nums[nums[i] - 1], nums[i] = nums[i], nums[nums[i] - 1]  # Swap
+        
+        # Step 2: Identify the first missing positive number
+        for i in range(n):
+            if nums[i] != i + 1:
+                return i + 1
+        
+        # If all numbers are in place, return the next number
+        return n + 1
