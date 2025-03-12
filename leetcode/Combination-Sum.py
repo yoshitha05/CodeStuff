746a1121493305c06cfcb8e396f7769189025143
YOSHITHA P <142377316+yoshitha05@users.noreply.github.com>
2025-03-12 09:38:50 +0530

    [39] [Time Beats: 86.79%] [Memory Beats: 19.40%] - LeetPush

---
+++ leetcode/Combination-Sum.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def combinationSum(self, candidates, target):
+        \\\
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        \\\
+        res = []
+
+        def backtrack(start, path, total):
+            if total == target:
+                res.append(list(path))
+                return
+            if total > target:
+                return
+
+            for i in range(start, len(candidates)):
+                path.append(candidates[i])
+                backtrack(i, path, total + candidates[i])  # Not i + 1 because we can reuse the same element
+                path.pop()  # Undo the choice
+
+        backtrack(0, [], 0)
+        return res
