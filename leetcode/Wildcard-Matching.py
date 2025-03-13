a35b11c765cf822bdd66305da66cb903ed876bb7
YOSHITHA P <142377316+yoshitha05@users.noreply.github.com>
2025-03-13 12:49:39 +0530

    Update Wildcard-Matching.py

--- leetcode/Wildcard-Matching.py
+++ leetcode/Wildcard-Matching.py
@@ -19,7 +19,7 @@ class Solution(object):
             for j in range(1, n + 1):
                 if p[j - 1] == s[i - 1] or p[j - 1] == '?':
                     dp[i][j] = dp[i - 1][j - 1]
-                elif p[j - 1] == '*':
+                elif p[j - 1] == '*': 
                     dp[i][j] = dp[i - 1][j] or dp[i][j - 1]
 
         return dp[m][n]
