8af8613ce0d6db9d3c57fe28f9ef0820fe23422b
YOSHITHA P <142377316+yoshitha05@users.noreply.github.com>
2025-03-15 21:00:40 +0530

    [145] [Time Beats: 100.00%] [Memory Beats: 17.99%] - LeetPush

---
+++ leetcode/Binary-Tree-Postorder-Traversal.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def postorderTraversal(self, root):
+        \\\
+        :type root: Optional[TreeNode]
+        :rtype: List[int]
+        \\\
+        res = []
+        stack = [(root, False)]
+
+        while stack:
+            node, visited = stack.pop()
+            if node:
+                if visited:
+                    res.append(node.val)
+                else:
+                    stack.append((node, True))
+                    stack.append((node.right, False))
+                    stack.append((node.left, False))
+
+        return res
