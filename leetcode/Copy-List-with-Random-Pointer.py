eda69d3c5984ff6bf80632da508fa5e9914c12a3
YOSHITHA P <142377316+yoshitha05@users.noreply.github.com>
2025-03-15 20:53:48 +0530

    [138] [Time Beats: 52.46%] [Memory Beats: 13.98%] - LeetPush

---
+++ leetcode/Copy-List-with-Random-Pointer.py
@@ -0,0 +1,34 @@
+\\\
+# Definition for a Node.
+class Node:
+    def __init__(self, x, next=None, random=None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+\\\
+
+class Solution(object):
+    def copyRandomList(self, head):
+        \\\
+        :type head: Node
+        :rtype: Node
+        \\\
+        if not head:
+            return None
+
+        old_to_new = {}
+
+        curr = head
+        while curr:
+            old_to_new[curr] = Node(curr.val)
+            curr = curr.next
+
+        curr = head
+        while curr:
+            if curr.next:
+                old_to_new[curr].next = old_to_new[curr.next]
+            if curr.random:
+                old_to_new[curr].random = old_to_new[curr.random]
+            curr = curr.next
+
+        return old_to_new[head]
