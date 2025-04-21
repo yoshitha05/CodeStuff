34d82ffbf2429918056d50e474ff33c4c8df677b
YOSHITHA <142377316+yoshitha05@users.noreply.github.com>
2025-04-21 18:12:35 +0530

    [167] [Time Beats: 14.79%] [Memory Beats: 11.40%] - LeetPush

---
+++ leetcode/Two-Sum-II---Input-Array-Is-Sorted.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def twoSum(self, numbers, target):
+        \\\
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        \\\
+        left, right = 0, len(numbers) - 1
+        while left < right:
+            current_sum = numbers[left] + numbers[right]
+            if current_sum == target:
+                return [left + 1, right + 1]
+            elif current_sum < target:
+                left += 1
+            else:
+                right -= 1
